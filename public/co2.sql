6a6960c8a12cb3cc49b6ff7a48fd3e5b6e472b3d
Andreas Kugel <devel@akugel.de>
2022-06-27 13:14:34 +0200

    database template

---
+++ public/co2.sql
@@ -0,0 +1,61 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server Version:               10.3.34-MariaDB-0ubuntu0.20.04.1 - Ubuntu 20.04
+-- Server Betriebssystem:        debian-linux-gnu
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Exportiere Struktur von Tabelle okl.districts
+DROP TABLE IF EXISTS `districts`;
+CREATE TABLE IF NOT EXISTS `districts` (
+  `name` tinytext NOT NULL,
+  `users` int(11) NOT NULL DEFAULT 0,
+  `mults` int(11) NOT NULL DEFAULT 0,
+  `co2total` decimal(20,6) NOT NULL DEFAULT 11.000000
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Exportiere Struktur von Tabelle okl.submissions
+DROP TABLE IF EXISTS `submissions`;
+CREATE TABLE IF NOT EXISTS `submissions` (
+  `user` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `co2total` decimal(20,6) NOT NULL DEFAULT 0.000000,
+  `sector1` decimal(20,6) NOT NULL,
+  `sector2` decimal(20,6) NOT NULL,
+  `sector3` decimal(20,6) NOT NULL,
+  `sector4` decimal(20,6) NOT NULL,
+  `sector5` decimal(20,6) NOT NULL,
+  `location` tinytext NOT NULL,
+  `mult` tinyint(4) NOT NULL DEFAULT 0,
+  `json` text DEFAULT NULL,
+  `code` tinytext DEFAULT NULL,
+  KEY `FK__users` (`user`),
+  CONSTRAINT `FK__users` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Exportiere Struktur von Tabelle okl.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `hash` tinytext NOT NULL,
+  KEY `Schlüssel 1` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
